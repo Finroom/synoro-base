6cc913e022620ca91b40dfaac84304d5142eb649
Derishev Ivan <110668615+musta-krakish@users.noreply.github.com>
2025-09-11 20:20:54 +0500

    fix: next build options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(ts|tsx|js|jsx)$/,
+      include: /supabase\/functions/,
+      use: "ignore-loader",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
